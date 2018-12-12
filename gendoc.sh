6ac9e8b82d290632e4b93c16dbdb47391c6820d6
Peter Schiansky <peter.schiansky@univiea.ac.at>
2018-12-12 12:05:43 +0100

    add documentation creation scripts

---
+++ gendoc.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+echo "Calling doxygen\n"
+doxygen doxygen_cagecontrol
+
+cd doc/latex
+
+for i in *.dot; do
+  echo "converting $i to ps"
+  dot -Tps2 ${i%%.*}.dot -o ${i%%.*}.ps
+  echo "converting ${i%%.*}.ps to pdf"
+  ps2pdf ${i%%.*}.ps
+done
+
+echo "Generating single pdf"
+make
+
+echo "Copying pdf to docdir"
+cp refman.pdf ../cagecontrol_doc.pdf
+
+echo "generating pdf pax"
+cd ..
+pdfannotextractor cagecontrol_doc.pdf
+#
+# Broken
+#
+#cd ../html
+#echo "Generating QT helpfile"
+#qhelpgenerator index.qhp -o ../cagecontrol_help.qch
+#cd ..
+#qcollectiongenerator cagecontrol_help.qhcp -o cagecontrol_help.qhc
---
+++ gendoc_fast.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+echo "Calling doxygen\n"
+doxygen doxygen_cagecontrol
+
+cd doc/latex
+
+echo "Generating single pdf"
+make
+
+echo "Copying pdf to docdir"
+cp refman.pdf ../cagecontrol_doc.pdf
+
+echo "generating pdf pax"
+cd ..
+pdfannotextractor cagecontrol_doc.pdf
+#
+# Broken
+#
+#cd ../html
+#echo "Generating QT helpfile"
+#qhelpgenerator index.qhp -o ../cagecontrol_help.qch
+#cd ..
+#qcollectiongenerator cagecontrol_help.qhcp -o cagecontrol_help.qhc
